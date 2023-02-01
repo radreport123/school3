5f5f7de0bf6fa5be2c60d8816a5ac103b2e0326b
Asahan Sahan <asahansahan@Asahans-MacBook-Pro-2.local>
2023-02-01 18:30:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const userRoutes = require('./routes/user');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/users', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'user.html'));
+});
+
+router.post('/users', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+  });
+
+module.exports = router;
